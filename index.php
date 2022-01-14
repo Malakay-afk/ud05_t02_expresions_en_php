e14360ad9dc124a9f7429b475076d5c9b97ec122
Miguel García <mikelrapsolo@gmail.com>
2022-01-14 09:25:07 +0100

    operadores aritméticos

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contruccións de expresións en php</title>
+</head>
+<body>
+    <h3>Contruccións de expresións en php</h3>
+<?php
+// Operadores aritméticos
+$x=10;
+$y=3;
+$z=$x*$y;
+echo $x. "x" .$y." = ".$z."<br/>";
+$z=$x%$y;
+echo $x."módulo".$y." = ".$z."<br/>"
+//Operadores de asignación * /
+?>
+</body>
+</html>
